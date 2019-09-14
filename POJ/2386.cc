d61a5b4251fe7fea50d7a3d637bee7bb6d948cef
Maxioo <cd84413336@163.com>
2019-09-14 18:26:18 +0800

    add POJ 2386

--- POJ/2386.cc
+++ POJ/2386.cc
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<fstream>
-#include <unistd.h>
-#include <stdio.h>
 
+const int DEBUG = 0;
 const int MAXNM = 100;
+
 char all_temp[MAXNM][MAXNM] = {0};
 void dfs(int n_site, int m_site, int N, int M){
     all_temp[n_site][m_site] = '.';
     for(int i = -1; i <=1; i++)
         for(int j = -1; j <= 1; j++){
-            if(n_site + i <= 0 | n_site + i >= N | m_site + j <= 0 | m_site + j >= M)
+            if(n_site + i < 0 | n_site + i >= N | m_site + j < 0 | m_site + j >= M)
                 continue;
             if(all_temp[n_site+i][m_site+j] == 'W')
                 dfs(n_site+i,m_site+j,N,M);
@@ -18,13 +18,14 @@ void dfs(int n_site, int m_site, int N, int M){
 
 void solve()
 {
-    std::ifstream fin("input");
-    std::streambuf *cinbackup;
-    cinbackup = std::cin.rdbuf(fin.rdbuf());
+    if(DEBUG){
+        std::ifstream fin("input");
+        std::streambuf *cinbackup;
+        cinbackup = std::cin.rdbuf(fin.rdbuf());
+    }
 
     int N, M;
     std::cin >> N >> M;
-    std::cout << N << M;
 
     char temp_char;
     for(int i = 0; i != N; i++)
@@ -35,8 +36,10 @@ void solve()
     int count = 0;
     for(int i = 0; i != N; i++)
         for(int j = 0; j != M; j++){
-            if(all_temp[i][j] == 'W')
+            if(all_temp[i][j] == 'W'){
+                dfs(i, j, N, M);
                 count++;
+            }
         }
     std::cout << count << std::endl;
 }
